0ad3e81c40f4ac1127fed9cb7c24e4be54582b2a
gulshalla <omarhassangul@gmail.com>
2019-07-15 11:12:29 +0530

    updated proc file

--- blog/feed.py
+++ blog/feed.py
@@ -13,8 +13,8 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        'FROM post p JOIN user u ON p.author_id = u.id'
+        'ORDER BY created DESC'
     ).fetchall()
     return render_template('feed/index.html', posts=posts)
 
@@ -47,8 +47,8 @@ def create():
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' WHERE p.id = ?',
+        'FROM post p JOIN user u ON p.author_id = u.id'
+        'WHERE p.id = ?',
         (id,)
     ).fetchone()
 
@@ -96,3 +96,4 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('feed.index'))
+
